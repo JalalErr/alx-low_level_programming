d9ee5dfde6f0506e165c1f0327f1e90c73bcb56d
JalalErr <jalal.errajy@gmail.com>
2024-02-04 21:27:49 +0100

    fixing task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - function that print t prints all the elements of a list_t list.
- * @h: it's a pointer of type list_t.
- * Return: the number of nodes in success.
- */
-
+  * print_list - Prints all elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of nodes
+  */
+ 
 size_t print_list(const list_t *h)
 {
-/* Variable declaration section: */
-size_t count = 0;
+	size_t count = 0;
 
-/* Code section: */
-while (h != NULL)
-{
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
-printf("[%d] %s \n", h->len, h->str);
-h = h->next;
-count++;
-}
-return (count);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
