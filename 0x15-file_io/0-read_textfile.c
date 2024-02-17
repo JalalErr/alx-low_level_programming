fe27b44080a2bd1755ac9f47204fb7f8593c473a
JalalErr <jalal.errajy@gmail.com>
2024-02-17 19:24:50 +0100

    Fixing code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - function that reads a text file & prints in standard output.
- * @filename: string take name of file.
- * @letters: nomber of letters you want to print.
- * Return: the actual number of letters it could read and print,
- * (0) if the file can not be opened or read.
- * or if filename is NULL,or if write fails or does not write.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-/* Variable declaration section: */
-ssize_t fd_o, fd_w, fd_r;
-char *buff;
+	ssize_t o, r, w;
+	char *buffer;
 
-/* Code section*/
-if (filename == NULL)
-return (0);
+	if (filename == NULL)
+		return (0);
 
-buff = malloc(sizeof(char) * letters);
-if (buff == NULL)
-return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-fd_o = open(filename, O_RDONLY);
-fd_r = read(fd_o, buff, letters);
-fd_w = write(STDOUT_FILENO, buff, fd_r);
-if (fd_o == -1 || fd_r == -1 || fd_w == -1 || fd_w != fd_r)
-free(buff);
-return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-free(buff);
-close(fd_o);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
 
-return (fd_w);
+	free(buffer);
+	close(o);
+
+	return (w);
 }
